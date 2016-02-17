11e1a8322355dbcd833a1c6e6fc05ba3b01de550
João Paulo Pozo Lindgren <jplindgrenbr@hotmail.com>
2016-02-16 21:32:03 -0800

    Add some logs to script build

--- build.bat
+++ build.bat
@@ -1,6 +1,12 @@
 @echo off
 cls
+
+ls
+
+echo "Installing Fake..."
 "tools\Nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
 
+echo "Fake Installed!!"
+ls
 "packages\FAKE\tools\Fake.exe" build.fsx
 pause
