324db11ca90cc70522ea1e0358dcf5a709e3673a
João Paulo Pozo Lindgren <jplindgrenbr@hotmail.com>
2016-02-16 21:10:10 -0800

    installing fake programatically from script instead of nuget. We use fake to trigger nuget!

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 @echo off
 cls
-"packages\FAKE.4.20.0\tools\Fake.exe" build.fsx
+"tools\Nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
+
+"packages\FAKE\tools\Fake.exe" build.fsx
 pause
